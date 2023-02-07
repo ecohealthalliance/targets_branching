30efba9f214a403c38fd4746cfc035b4ab8b4219
Nathan Layman <layman@ecohealthalliance.org>
2023-02-07 12:28:55 -0800

    Add branching overview presentation to reports

--- _targets.R
+++ _targets.R
@@ -1,91 +1,61 @@
-################################################################################
-#
-# Project build script
-#
-################################################################################
-
-# Load packages (in packages.R) and load project-specific functions in R folder
-suppressPackageStartupMessages(source("packages.R"))
-for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
+library(targets)
+library(tarchetypes)
+library(tidyverse)
 
 # Set build options ------------------------------------------------------------
+set.seed(2)
+future::plan(future.callr::callr, workers = 5)
+
+# GLOBAL VARIABLE
+iris_species <- tibble(sp = c("setosa", "versicolor", "virginica"))
 
 
-# Groups of targets ------------------------------------------------------------
-
-## Data input
+# TARGET BASED VARIABLE
 data_input_targets <- tar_plan(
-  ## Example data input target/s; delete and replace with your own data input
-  ## targets
+  tar_target(iris, iris |> mutate(Species = as.character(Species)))
   
-  targets::tar_target("example_target",create_example_target(x = TRUE))
+  # tar_target(iris, rbind(iris |> mutate(Species = as.character(Species)), 
+  #                        c(6.0, 3.0, 5.1, 1.8, "nathanii")))
+)
+
+static_branching <- tar_plan(
+  tar_map(
+    values = iris_species,
+    names = sp, 
+    
+    tar_target(group_split, iris |> filter(Species == sp)),
+    tar_target(apply, group_split |> mutate(Species = toupper(Species)))
+  )
 )
 
 
-## Data processing
-data_processing_targets <- tar_plan(
-  ## Example data processing target/s; delete and replace with your own data
-  ## processing targets
+dynamic_branching <- tar_plan(
+  
+  # Iteration here generates a branch for each species group
+  tar_target(group_split, 
+             iris |> 
+               group_by(Species) |> 
+               tar_group(),
+             iteration = "group"),
+  
+  tar_target(apply_combine, group_split |> mutate(Spieces = toupper(Species)),
+             pattern = map(group_split),
+             iteration = "vector")
+  
+  # Interactive questions time.
+  # Q: What happens when we change iteration? Or leave it out? 
+  # Q: How much gets re-done when we do this? All targets? None? Let's tar_read and find out.
+  # Q: What happens with vector iteration with non-conformable results?
+  # Q: What happens if I add, remove, or fix a row in sampling_schemes? How much gets redone? 
+  # Q: Does it depend on where I do it?
   
 )
 
-
-## Analysis
-analysis_targets <- tar_plan(
-  ## Example analysis target/s; delete and replace with your own analysis
-  ## targets
- 
-)
-
-## Outputs
-outputs_targets <- tar_plan(
-  ## This is a placeholder for any targets that produces outputs such as
-  ## tables of model outputs, plots, etc. Delete or keep empty if you will not
-  ## produce any of these types of outputs
-)
-
-
-## Report
-report_targets <- tar_plan(
-  ## Example Rmarkdown report target/s; delete and replace with your own
-  ## Rmarkdown report target/s
-  
-  # tar_render(
-  #   example_report, path = "reports/example_report.Rmd", 
-  #   output_dir = "outputs", knit_root_dir = here::here()
-  # )
-)
-
-## Deploy targets
-deploy_targets <- tar_plan(
-  ## This is a placeholder for any targets that are meant to deploy reports or
-  ## any outputs externally e.g., website, Google Cloud Storage, Amazon Web
-  ## Services buckets, etc. Delete or keep empty if you will not perform any
-  ## deployments. The aws_s3_upload function requires AWS credentials to be loaded
-  ## but will print a warning and do nothing if not
-  
-  # html_files = containerTemplateUtils::get_file_paths(tar_obj = example_report,
-  #                                                     pattern = "\\.html$"),
-  # uploaded_report = containerTemplateUtils::aws_s3_upload(html_files,
-  #                                                       bucket = Sys.getenv("AWS_BUCKET"),
-  #                                                       error = FALSE,
-  #                                                       file_type = "html"),
-  # email_updates= 
-  #   containerTemplateUtils::send_email_update(
-  #     to = strsplit(Sys.getenv("EMAIL_RECIPIENTS"),";")[[1]],
-  #     from = Sys.getenv("EMAIL_SENDER"),
-  #     project_name = "My Project",
-  #     attach = TRUE
-  #   )
-)
 
 # List targets -----------------------------------------------------------------
 
 list(
   data_input_targets,
-  data_processing_targets,
-  analysis_targets,
-  outputs_targets,
-  report_targets,
-  deploy_targets
+  static_branching,
+  dynamic_branching
 )
